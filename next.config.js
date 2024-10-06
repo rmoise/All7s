c1e2aa5d0d9434807c32b43d817dd1e393e867c1
Roderick Moise <roderickfmoise@gmail.com>
2024-10-06 21:08:09 +0200

    update dependencies

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ const nextConfig = {
   swcMinify: true,
 
   env: {
-    // Making sure these environment variables are passed down to the client
+    // Providing fallbacks in case environment variables are not defined
     NEXT_PUBLIC_SANITY_DATASET:
       process.env.NEXT_PUBLIC_ENVIRONMENT === 'staging'
-        ? process.env.NEXT_STAGING_SANITY_DATASET
-        : process.env.NEXT_PUBLIC_SANITY_DATASET,
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+        ? process.env.NEXT_STAGING_SANITY_DATASET || 'default-staging-dataset'
+        : process.env.NEXT_PUBLIC_SANITY_DATASET || 'default-production-dataset',
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'default-project-id',
     NEXT_PUBLIC_SANITY_TOKEN:
       process.env.NEXT_PUBLIC_ENVIRONMENT === 'staging'
-        ? process.env.NEXT_STAGING_SANITY_TOKEN
-        : process.env.NEXT_PUBLIC_SANITY_TOKEN,
+        ? process.env.NEXT_STAGING_SANITY_TOKEN || 'default-staging-token'
+        : process.env.NEXT_PUBLIC_SANITY_TOKEN || 'default-production-token',
   },
 
   images: {
