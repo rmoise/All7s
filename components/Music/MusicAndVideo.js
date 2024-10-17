198c89f4462c4292d54a48d0a6ae1fbc5bf83b37
Roderick Moise <roderickfmoise@gmail.com>
2024-10-17 13:36:49 +0200

    Ensured background video loops and plays inline on mobile.

--- components/Music/MusicAndVideo.js
+++ components/Music/MusicAndVideo.js
@@ -190,6 +190,7 @@ export default function MusicAndVideo({ videoPreLink }) {
             muted
             autoPlay
             loop
+            playsInline
             controls={false}
           />
         </div>
