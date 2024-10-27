dc9c504998bbc0f2c100188cc59f7ef99913fe36
Roderick Moise <roderickfmoise@gmail.com>
2024-10-27 12:38:33 +0100

    Update Sanity client setup and add error handling for test route

--- pages/api/testSiteSettings.js
+++ pages/api/testSiteSettings.js
@@ -1,16 +1,19 @@
+// /pages/api/testSiteSettings.js
 import { client } from '../../lib/client';
 
 export default async function handler(req, res) {
-  const siteSettingsQuery = `*[_type == "settings"][0]{
-    title,
-    seo { metaTitle, metaDescription, openGraphImage { asset-> { url } } }
-  }`;
+  const query = `*[_type == "settings"][0]`;
+
   try {
-    const siteSettings = await client.fetch(siteSettingsQuery);
-    console.log("Fetched Site Settings in Production:", siteSettings);
-    res.status(200).json({ siteSettings });
+    const result = await client.fetch(query);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: "Settings document not found" });
+    }
   } catch (error) {
-    console.error("Error fetching site settings:", error);
-    res.status(500).json({ error: error.message });
+    console.error("Error fetching site settings:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
