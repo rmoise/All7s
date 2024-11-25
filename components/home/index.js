2a69267dea5dd8fe50402236f84d935953ad72aa
Roderick Moise <roderickfmoise@gmail.com>
2024-11-25 01:20:37 +0100

    fix: add unoptimized prop to FlipCard images to fix production image loading

--- components/home/index.js
+++ components/home/index.js
@@ -1,5 +1,4 @@
 export { default as HeroBanner } from './HeroBanner';
 export { default as Featured } from './Featured';
 export { default as About } from './About';
-export { default as Album } from './Album';
 export { default as Splash } from './Splash';
